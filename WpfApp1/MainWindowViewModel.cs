596c1e79e7842b1d8e1c931f84c3ec22e987c75e
Daniel Balogh <daniel.balogh@ssi-schaefer.com>
2022-01-10 11:00:56 +0100

    Projektdateien hinzufügen.

---
+++ WpfApp1/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+  class MainWindowViewModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private string manageString;
+    private string fromManageString;
+    private string homeString;
+
+    public string ManageString
+    {
+      get
+      {
+        return manageString;
+      }
+      set
+      {
+        manageString = value;
+        NotifyPropetyChanged();
+      }
+    }
+
+    public string FromManageString
+    {
+      get
+      {
+        return fromManageString;
+      }
+      set
+      {
+        fromManageString = value;
+        NotifyPropetyChanged();
+      }
+    }
+
+    public string HomeString
+    {
+      get
+      {
+        return homeString;
+      }
+      set
+      {
+        homeString = value;
+        NotifyPropetyChanged();
+      }
+    }
+
+
+    public void NotifyPropetyChanged([CallerMemberName] string propertyName = "")
+    {
+      if (propertyName.Equals(nameof(ManageString)))
+      {
+        FromManageString = "From Manage Tab: " + ManageString;
+      }
+
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
